b3e22786a0f4937fbd19db02379a92bc95d1d9d2
Nguyễn Công Huy <huycon205@gmail.com>
2025-02-24 11:55:23 +0700

    Include loss functions and xor examples

--- dense.py
+++ dense.py
@@ -4,9 +4,8 @@ class Dense(Layer):
     def __init__(self, input_size, output_size):
         self.weights = np.random.rand(output_size, input_size)
         self.bias = np.random.rand(output_size, 1)
-    def forward(self, output):
-        self.input = input
-        return np.dot(self.weights, self.input) + self.bias
+    def forward(self, input):
+        return np.dot(self.weights, input) + self.bias
     def backward(self, output_gradient, learning_rate):
         weights_gradient = np.dot(output_gradient, self.input.T)
         self.weights -= learning_rate*weights_gradient
